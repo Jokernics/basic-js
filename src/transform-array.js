7b05f0f0039b16f7f7fa95cde4aae8b211266969
egor <spirin.egor13@gmail.com>
2022-03-21 00:59:55 +0300

    transform arrray done

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,52 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!(arr instanceof Array)) throw Error("'arr' parameter must be an instance of the Array!")
+  let copy = [...arr]
+  function recurse() {
+    for (let i = 0; i < copy.length; i++) {
+      switch (copy[i]) {
+        case '--discard-next':
+          if (copy[i + 1]) {
+            copy.splice(i, 2, 'error')
+            recurse()
+          } else {
+            copy.splice(i, 1)
+          }
+          continue
+        case '--discard-prev':
+          if (copy[i - 1]) {
+            copy.splice(i-1, 2, 'error')
+            recurse()
+          } else {
+            copy.splice(i, 1)
+          }
+          continue
+        case '--double-next':
+          if (copy[i + 1]) {
+            copy.splice(i, 1, copy[i+1])
+            recurse()
+          } else {
+            copy.splice(i, 1)
+          }
+          continue
+        case '--double-prev':
+          if (copy[i - 1]) {
+            copy.splice(i, 1, copy[i -1])
+            recurse()
+          } else {
+            copy.splice(i, 1)
+          }
+          continue
+        default:
+          continue
+      }
+    }
+    return copy
+  }
+  
+  return recurse().filter( item => item !== 'error')
 }
 
 module.exports = {
