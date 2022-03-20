a208ec48000c025803a1f660281c968f7d7fc373
egor <spirin.egor13@gmail.com>
2022-03-20 18:47:04 +0300

    in procces

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,15 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (!Number.isNaN(+sampleActivity) && typeof sampleActivity === 'string' && typeof sampleActivity !== null && typeof sampleActivity !== undefined && sampleActivity.trim().length > 0) {
+    const k = 0.693 / HALF_LIFE_PERIOD
+    let result = Math.ceil(Math.log(MODERN_ACTIVITY/sampleActivity) / k)
+    if (result === Infinity) return false
+    return result > 0 ? result : false
+  } else {
+    return false
+  }
 }
 
 module.exports = {
